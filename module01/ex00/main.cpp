2acf0506e7eb70151dbe7c6e12286d6955850cad
Ahmad Hammoudeh <hammoudeh.ahmad@hotmail.com>
2022-05-24 13:18:08 +0400

    Made random

--- module01/ex00/main.cpp
+++ module01/ex00/main.cpp
@@ -6,7 +6,7 @@
 /*   By: ahhammou <ahhammou@student.42abudhabi.a    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/17 14:03:23 by ahhammou          #+#    #+#             */
-/*   Updated: 2022/05/17 15:11:39 by ahhammou         ###   ########.fr       */
+/*   Updated: 2022/05/24 02:06:10 by ahhammou         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,6 +19,32 @@ Zombie *newZombie(std::string name){
 	add->nameing(name);
 	add->announce();
 	return (add);
+}
+
+std::string randomName(int length) {
+
+  char consonents[] = {'b','c','d','f','g','h','j','k','l','m','n','p','q','r','s','t','v','w','x','z'};
+  char vowels[] = {'a','e','i','o','u','y'};
+
+  std::string name = "";
+
+  int random = rand() % 2;
+  int count = 0;
+
+  for(int i = 0; i < length; i++) {
+
+    if(random < 2 && count < 2) {
+      name = name + consonents[rand() % 19];
+      count++;
+    }
+    else {
+      name = name + vowels[rand() % 5];
+      count = 0;
+    }
+    random = rand() % 2;
+  }
+
+  return name;
 }
 
 void randomChump(std::string name){
@@ -34,6 +60,6 @@ int main()
 	Zombie *a;
 	
 	a = newZombie("ahmad");
-	randomChump("Bosi");
+	randomChump(randomName(5));
 	a->~Zombie();
 }
--- module01/ex01/Zombie.cpp
+++ module01/ex01/Zombie.cpp
@@ -1,12 +1,12 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   Zomviw.cpp                                         :+:      :+:    :+:   */
+/*   Zombie.cpp                                         :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: ahhammou <ahhammou@student.42abudhabi.a    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/17 14:09:49 by ahhammou          #+#    #+#             */
-/*   Updated: 2022/05/17 14:17:46 by ahhammou         ###   ########.fr       */
+/*   Updated: 2022/05/24 02:04:47 by ahhammou         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,4 +18,30 @@ void Zombie::announce(void){
 
 void Zombie::nameing(std::string name){
 	this->name = name;
+}
+
+std::string randomName(int length) {
+
+  char consonents[] = {'b','c','d','f','g','h','j','k','l','m','n','p','q','r','s','t','v','w','x','z'};
+  char vowels[] = {'a','e','i','o','u','y'};
+
+  std::string name = "";
+
+  int random = rand() % 2;
+  int count = 0;
+
+  for(int i = 0; i < length; i++) {
+
+    if(random < 2 && count < 2) {
+      name = name + consonents[rand() % 19];
+      count++;
+    }
+    else {
+      name = name + vowels[rand() % 5];
+      count = 0;
+    }
+    random = rand() % 2;
+  }
+
+  return name;
 }
